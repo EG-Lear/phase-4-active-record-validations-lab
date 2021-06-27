6b3d54ff31c14f65ae081c6a1b24bde96e6f10cb
Garret <gwiats@gmail.com>
2021-06-27 12:55:33 -0400

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,17 @@
 class Post < ApplicationRecord
+  validates :title, presence: true
+  validates :content, length: { minimum: 250 }
+  validates :summary, length: { maximum: 250 }
+  validates :category, inclusion: { in: %w(Fiction Non-Fiction) } 
+  validate :my_method
+
+  def my_method
+    if title.present?
+      tester = title.split
+      if tester.include?("Won't") && tester.include?("Believe")
+      else
+        errors.add(:title, "Clickbait Please")
+      end
+    end
+  end
 end
